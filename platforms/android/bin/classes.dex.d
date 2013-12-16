cb21bef833f0a89dd8ec43cb5a7e1a5aa71db574
William Agay <me@williamagay.fr>
2013-12-16 20:32:25 +0100

    Cordova_plugins.js update

--- platforms/android/bin/classes.dex.d
+++ platforms/android/bin/classes.dex.d
@@ -9,6 +9,9 @@
 /Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/com/chariotsolutions/nfc/plugin/NfcPlugin$8.class \
 /Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/com/chariotsolutions/nfc/plugin/NfcPlugin.class \
 /Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/com/chariotsolutions/nfc/plugin/Util.class \
+/Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/com/chariotsolutions/toast/plugin/ToastPlugin$1.class \
+/Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/com/chariotsolutions/toast/plugin/ToastPlugin$2.class \
+/Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/com/chariotsolutions/toast/plugin/ToastPlugin.class \
 /Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/com/hunear/nfcdemo/BuildConfig.class \
 /Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/com/hunear/nfcdemo/NFCDemo.class \
 /Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/com/hunear/nfcdemo/R$attr.class \
@@ -17,4 +20,6 @@
 /Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/com/hunear/nfcdemo/R$string.class \
 /Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/com/hunear/nfcdemo/R$xml.class \
 /Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/com/hunear/nfcdemo/R.class \
+/Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/org/apache/cordova/api/Dummy.class \
+/Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes/org/apache/cordova/vibration/Vibration.class \
 /Users/williamagay/Apps/NFCDemo/platforms/android/bin/dexedLibs/cordova-3.0.0-a5532df99cc46dd0c0b1d62747a0f09b.jar \
