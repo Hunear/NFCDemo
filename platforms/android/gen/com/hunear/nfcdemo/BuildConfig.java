1137efd82c424c10593998ad89e15cd1e245d9ba
William Agay <me@williamagay.fr>
2013-12-16 19:27:58 +0100

    clean repo

--- platforms/android/gen/com/hunear/nfcdemo/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.hunear.nfcdemo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
