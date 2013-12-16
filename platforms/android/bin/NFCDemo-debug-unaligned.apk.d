75e038129f7f8805645502eedb94608f649fbc15
William Agay <me@williamagay.fr>
2013-12-16 20:01:22 +0100

    Build

---
+++ platforms/android/bin/NFCDemo-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/williamagay/Apps/NFCDemo/platforms/android/bin/NFCDemo-debug-unaligned.apk : \
+/Users/williamagay/Apps/NFCDemo/platforms/android/bin/NFCDemo.ap_ \
+/Users/williamagay/Apps/NFCDemo/platforms/android/bin/classes.dex \
+/Users/williamagay/Apps/NFCDemo/platforms/android/libs/cordova-3.0.0.jar \
